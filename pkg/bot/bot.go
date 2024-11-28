5a97c2c77a86da2f8e12a96bd2d641bb5a29683b
RG1ee <niwows@yandex.ru>
2024-11-28 17:24:19 +0300

    add start handler

--- pkg/bot/bot.go
+++ pkg/bot/bot.go
@@ -5,6 +5,7 @@ import (
 
 	"log/slog"
 
+	ph "github.com/RG1ee/gobot/pkg/bot/handlers/public"
 	"github.com/spf13/viper"
 	tele "gopkg.in/telebot.v3"
 )
@@ -26,7 +27,7 @@ func createBot() (*tele.Bot, error) {
 	pref := tele.Settings{
 		Token:   viper.GetString("TOKEN"),
 		Poller:  &tele.LongPoller{Timeout: 10 * time.Second},
-		Verbose: viper.GetBool("DEBUG"),
+		Verbose: viper.GetBool("VERBOSE"),
 	}
 
 	bot, err := tele.NewBot(pref)
@@ -37,11 +38,18 @@ func createBot() (*tele.Bot, error) {
 	return bot, nil
 }
 
+func (tb *TelegramBot) registerHandler() {
+	public := tb.Bot.Group()
+	public.Handle("/start", ph.HandleStart())
+}
+
 func Start() error {
 	tb, err := NewTelegramBot()
 	if err != nil {
 		return err
 	}
+
+	tb.registerHandler()
 
 	slog.Info("Bot started")
 	tb.Bot.Start()
