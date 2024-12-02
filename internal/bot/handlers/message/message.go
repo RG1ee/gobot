deef01255a10d27638af31b619fad6155d5e6126
RG1ee <niwows@yandex.ru>
2024-12-02 16:41:17 +0300

    test handler on photo with caption

--- internal/bot/handlers/message/message.go
+++ internal/bot/handlers/message/message.go
@@ -1,8 +1,6 @@
 package message
 
 import (
-	"fmt"
-
 	"github.com/RG1ee/gobot/internal/bot/keyboards/reply"
 	stateconst "github.com/RG1ee/gobot/internal/bot/state_const"
 	"github.com/avi-gecko/fsm/pkg/fsm"
@@ -15,11 +13,22 @@ func StartMessageHandler(c tele.Context) error {
 
 func WriteNewClothMessageHandler(c tele.Context) error {
 	fsm := c.Get("fsm").(fsm.FSM)
-	fsm.SetState(uint64(c.Chat().ID), stateconst.StateSendCloth)
-	newState, err := fsm.GetState(uint64(c.Chat().ID))
-	if err != nil {
-		return err
-	}
+	fsm.SetState(uint64(c.Chat().ID), stateconst.StateWaitPhoto)
 
-	return c.Send(fmt.Sprint(newState))
+	return c.Send("Отправьте фото")
+}
+
+func GetPhotoClothMessageHandler(c tele.Context) error {
+	// fsm := c.Get("fsm").(fsm.FSM)
+	// fsm.SetState(uint64(c.Chat().ID), stateconst.StateWaitPhoto)
+
+	// TODO: Add to database photoID and caption
+
+	// NOTE: Get photo ID
+	// photoId := c.Message().Photo.FileID
+
+	// NOTE: Get caption
+	// messageText := c.Message().Caption
+
+	return c.Send("Hello")
 }
