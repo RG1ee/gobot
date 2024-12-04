2bdd5ca4a3f5fa18c8e89726f96b4a3c135c0260
Алексей Ильин <thealexsnare@yandex.ru>
2024-12-04 21:40:22 +0300

    Add middlware for repository

--- pkg/middleware/middleware.go
+++ pkg/middleware/middleware.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"github.com/RG1ee/gobot/pkg/domain"
 	"github.com/avi-gecko/fsm/pkg/fsm"
 	tele "gopkg.in/telebot.v3"
 )
@@ -9,6 +10,15 @@ func FsmMiddleware(fsm fsm.FSM) func(next tele.HandlerFunc) tele.HandlerFunc {
 	return func(next tele.HandlerFunc) tele.HandlerFunc {
 		return func(c tele.Context) error {
 			c.Set("fsm", fsm)
+			return next(c)
+		}
+	}
+}
+
+func Repository(db_backend domain.Cloth) func(next tele.HandlerFunc) tele.HandlerFunc {
+	return func(next tele.HandlerFunc) tele.HandlerFunc {
+		return func(c tele.Context) error {
+			c.Set("repository", db_backend)
 			return next(c)
 		}
 	}
