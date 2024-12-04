781f2225477e23c938b1e13fca072625835060d7
Алексей Ильин <thealexsnare@yandex.ru>
2024-12-04 21:39:49 +0300

    Add repo for cloth

---
+++ internal/repository/repository.go
@@ -0,0 +1,10 @@
+package repository
+
+import "github.com/RG1ee/gobot/pkg/domain"
+
+type Cloth interface {
+	GetIncoming() []domain.Cloth
+	GetOutgoing() []domain.Cloth
+	Insert(domain.Cloth)
+	Out(name string) error
+}
