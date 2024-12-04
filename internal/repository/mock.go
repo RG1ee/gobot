86a91493fd264b6aa9dd010639e27600b875e909
Алексей Ильин <thealexsnare@yandex.ru>
2024-12-04 21:40:47 +0300

    Add mock for repo

---
+++ internal/repository/mock.go
@@ -0,0 +1,30 @@
+package repository
+
+import (
+	"time"
+
+	"github.com/RG1ee/gobot/pkg/domain"
+)
+
+type Mock struct {
+}
+
+func (Mock) GetIncoming() []domain.Cloth {
+	r := make([]domain.Cloth, 1)
+	r[0] = domain.Cloth{Name: "test", PhotoId: "123", IncomingDate: time.Now(), OutgoingDate: time.Now(), Status: domain.ClothIncoming} 
+	return r
+}
+
+func (Mock) GetOutgoing() []domain.Cloth {
+	r := make([]domain.Cloth, 1)
+	r[0] = domain.Cloth{Name: "test", PhotoId: "123", IncomingDate: time.Now(), OutgoingDate: time.Now(), Status: domain.ClothOutgoing} 
+	return r
+}
+
+func (Mock) Insert() {
+
+}
+
+func (Mock) Out(name string) error{
+	return nil
+}
