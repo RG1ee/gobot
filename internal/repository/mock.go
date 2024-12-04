93f9285b72632fbc68d4bd2b1f1eafa4ea955e21
Алексей Ильин <thealexsnare@yandex.ru>
2024-12-04 21:51:32 +0300

    fix mock

--- internal/repository/mock.go
+++ internal/repository/mock.go
@@ -11,20 +11,20 @@ type Mock struct {
 
 func (Mock) GetIncoming() []domain.Cloth {
 	r := make([]domain.Cloth, 1)
-	r[0] = domain.Cloth{Name: "test", PhotoId: "123", IncomingDate: time.Now(), OutgoingDate: time.Now(), Status: domain.ClothIncoming} 
+	r[0] = domain.Cloth{Name: "test", PhotoId: "123", IncomingDate: time.Now(), OutgoingDate: time.Now(), Status: domain.ClothIncoming}
 	return r
 }
 
 func (Mock) GetOutgoing() []domain.Cloth {
 	r := make([]domain.Cloth, 1)
-	r[0] = domain.Cloth{Name: "test", PhotoId: "123", IncomingDate: time.Now(), OutgoingDate: time.Now(), Status: domain.ClothOutgoing} 
+	r[0] = domain.Cloth{Name: "test", PhotoId: "123", IncomingDate: time.Now(), OutgoingDate: time.Now(), Status: domain.ClothOutgoing}
 	return r
 }
 
-func (Mock) Insert() {
+func (Mock) Insert(domain.Cloth) {
 
 }
 
-func (Mock) Out(name string) error{
+func (Mock) Out(name string) error {
 	return nil
 }
