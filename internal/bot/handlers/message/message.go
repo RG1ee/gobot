b99116b732f866d60ee4fc6d5b7ada2eec2b1c14
RG1ee <niwows@yandex.ru>
2024-12-05 02:07:16 +0300

    add handlers for get list incoming cloth and connect to db

--- internal/bot/handlers/message/message.go
+++ internal/bot/handlers/message/message.go
@@ -2,9 +2,12 @@ package message
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/RG1ee/gobot/internal/bot/keyboards/reply"
 	stateconst "github.com/RG1ee/gobot/internal/bot/state_const"
+	"github.com/RG1ee/gobot/internal/repository"
+	"github.com/RG1ee/gobot/pkg/domain"
 	"github.com/avi-gecko/fsm/pkg/fsm"
 	tele "gopkg.in/telebot.v3"
 )
@@ -25,7 +28,7 @@ func CancelHandler(c tele.Context) error {
 	_, err := fsm.GetState(uint64(c.Chat().ID))
 
 	if err != nil {
-		return c.Send("Отменять нечего :)")
+		return c.Send("Отменять нечего :)", reply.StartKeyboard())
 	}
 
 	fsm.ClearState(uint64(c.Chat().ID))
@@ -34,14 +37,31 @@ func CancelHandler(c tele.Context) error {
 }
 
 func GetPhotoClothMessageHandler(c tele.Context) error {
-	// TODO: Add to database photoID and caption
-	// NOTE: Get photo ID
-	// photoId := c.Message().Photo.FileID
+	db := c.Get("repository").(repository.Cloth)
+	fsm := c.Get("fsm").(fsm.FSM)
 
+	photoId := c.Message().Photo.FileID
 	captionText := c.Message().Caption
 	if captionText == "" {
 		return c.Send("Отправьте фотографию с подписью")
 	}
 
-	return c.Send("Сохранил "+fmt.Sprint(captionText), reply.StartKeyboard())
+	insertData := domain.Cloth{
+		Name:         captionText,
+		PhotoId:      photoId,
+		IncomingDate: time.Now(),
+		Status:       domain.ClothIncoming,
+	}
+	db.Insert(insertData)
+	fsm.ClearState(uint64(c.Chat().ID))
+	return c.Send("Вещь "+fmt.Sprint(captionText)+" отправлена", reply.StartKeyboard())
+}
+
+func GetListIncomingClothMessageHandler(c tele.Context) error {
+	db := c.Get("repository").(repository.Cloth)
+
+	allCloth := db.GetIncoming()
+	photo := &tele.Photo{File: tele.File{FileID: allCloth[0].PhotoId}, Caption: allCloth[0].Name}
+	_, err := photo.Send(c.Bot(), c.Chat(), &tele.SendOptions{ReplyMarkup: reply.CancelKeyboard()})
+	return err
 }
